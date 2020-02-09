4509f399f742f54358c9d9244c99d017acec926e
joker7t <batoan131196@gmail.com>
2020-02-10 00:14:21 +0700

    add redirect when login in app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { HOME_PATH, LOGIN_PATH, REGISTER_PATH } from './utils/Constant';
@@ -6,21 +6,35 @@ import Landing from './components/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import 'semantic-ui-css/semantic.min.css';
+import firebase from './firebase';
+import createHistory from './history';
 
-function App() {
+class App extends Component {
+  componentDidMount() {
+    firebase
+      .auth()
+      .onAuthStateChanged(user => {
+        if (user) {
+          createHistory.push(HOME_PATH);
+        }
+      })
+  }
 
-  return (
-    // <Provider store={store}>
-    <Router>
-      <div className="App">
-        <Route exact path={HOME_PATH} component={Landing} />
-        <Route exact path={LOGIN_PATH} component={Login} />
-        <Route exact path={REGISTER_PATH} component={Register} />
-      </div>
-    </Router>
-    // </Provider>
-  );
+  render() {
 
+    return (
+      // <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Route exact path={HOME_PATH} component={Landing} />
+          <Route exact path={LOGIN_PATH} component={Login} />
+          <Route exact path={REGISTER_PATH} component={Register} />
+        </div>
+      </Router>
+      // </Provider>
+    );
+
+  }
 }
 
 export default App;
