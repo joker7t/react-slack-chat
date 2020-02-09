8a09b7cab16895c7afb3d01c61a37e5272bc6770
joker7t <batoan131196@gmail.com>
2020-02-09 16:12:14 +0700

    build register

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
+import { Link } from "react-router-dom";
+import { LOGIN_PATH } from "../../utils/Constant";
 
 class Register extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            'username': '',
+            'email': '',
+            'password': '',
+            'confirmedPassword': ''
+        };
+    }
+
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+    };
+
     render() {
         return (
-            <div>
-                Register
-            </div>
+            <Grid textAlign="center" verticalAlign="middle" className="app">
+                <Grid.Column className="max-width-grid-column">
+                    <Header as="h2" icon color="orange" textAlign="center">
+                        <Icon name="puzzle piece" color="orange" />
+                        Register for chat
+                    </Header>
+                    <Form size="large" onSubmit={this.onSubmit}>
+                        <Segment stacked>
+                            <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="username"
+                                onChange={this.onChange} type="text" value={this.state.username} />
+                            <Form.Input fluid name="email" icon="email" iconPosition="left" placeholder="email address"
+                                onChange={this.onChange} type="text" value={this.state.email} />
+                            <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="password"
+                                onChange={this.onChange} type="password" value={this.state.password} />
+                            <Form.Input fluid name="confirmedPassword" icon="repeat" iconPosition="left" placeholder="password confirmation"
+                                onChange={this.onChange} type="password" value={this.state.confirmedPassword} />
+                            <Button className="ui button" type="submit">Submit</Button>
+                        </Segment>
+                    </Form>
+
+                    <Message>
+                        Already a user? <Link to={LOGIN_PATH}>Login</Link>
+                    </Message>
+                </Grid.Column>
+            </Grid>
         );
     }
 }
