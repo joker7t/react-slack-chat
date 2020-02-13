dc07ca2868c470e1838cb9f60133dfd1e7f80673
joker7t <batoan131196@gmail.com>
2020-02-13 20:41:41 +0700

    add basic components and layout

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { Grid } from "semantic-ui-react";
+import ColorPanel from './color-panel/ColorPanel';
+import SidePanel from './side-panel/SidePanel';
+import Messages from './messages/Messages';
+import MetaPanel from './meta-panel/MetaPanel';
 
 class Landing extends Component {
     render() {
         return (
-            <div>
-                Landing page
-            </div>
+            <Grid columns="equal" className="app main-background">
+
+                <ColorPanel />
+
+                <SidePanel />
+
+                <Grid.Column style={{ marginLeft: 320 }}>
+                    <Messages />
+                </Grid.Column>
+
+                <Grid.Column width={4}>
+                    <MetaPanel />
+                </Grid.Column>
+
+            </Grid>
         );
     }
 }
