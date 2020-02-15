b1fb3db5e4c037c2a70f2ecfc488c0d330c4d8b3
joker7t <batoan131196@gmail.com>
2020-02-15 07:00:58 +0700

    add action handler for channel except add

--- src/components/side-panel/Channels.js
+++ src/components/side-panel/Channels.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
+import { Menu, Icon, Modal, Form, Input, Button, Message } from "semantic-ui-react";
+import firebase from "../../firebase";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../Spinner";
 
 class Channels extends Component {
     constructor() {
@@ -8,12 +13,14 @@ class Channels extends Component {
             channels: [],
             modal: false,
             channelName: '',
-            channelDetails: ''
+            channelDetails: '',
+            isLoading: false,
+            isInValid: false
         };
     }
 
     handleCloseModal = () => {
-        this.setState({ modal: false });
+        this.setState({ modal: false, isInValid: false });
     }
 
     handleOpenModal = () => {
@@ -24,8 +31,51 @@ class Channels extends Component {
         this.setState({ [e.target.name]: e.target.value });
     };
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        if (this.isFormValid(this.state)) {
+            this.setState({ isInValid: false });
+            this.saveChannel();
+        } else {
+            this.setState({ isInValid: true });
+        }
+    }
+
+    isFormValid = ({ channelName, channelDetails }) => channelName && channelDetails;
+
+    saveChannel = async () => {
+        this.setState({ isLoading: true });
+        const channelRef = firebase.database().ref('channels');
+        const key = channelRef.push();
+        const { channelName, channelDetails } = this.state;
+        const { displayName, photoURL } = this.props.user;
+        const newChannel = {
+            id: key,
+            name: channelName,
+            details: channelDetails,
+            createBy: {
+                name: displayName,
+                avatar: photoURL
+            }
+        };
+
+        // .child(createdUser.user.uid).set({
+        //     name: createdUser.user.displayName,
+        //     avatar: createdUser.user.photoURL
+        // });
+        this.setState({ isLoading: false });
+    }
+
+    isShownErrorMessage = () => {
+        if (this.state.isInValid) {
+            return <Message error>
+                <p>Please fullfill all the channal data</p>
+            </Message>;
+        }
+    }
+
     render() {
-        const { channels, modal, channelName, channelDetails } = this.state;
+        const { channels, modal, channelName, channelDetails, isInValid, isLoading } = this.state;
         return (
             <React.Fragment>
                 <Menu.Menu style={{ paddingBottom: "2rem" }}>
@@ -38,15 +88,20 @@ class Channels extends Component {
                     </Menu.Item>
                 </Menu.Menu>
 
+
                 <Modal basic open={modal} onClose={this.handleCloseModal}>
+                    {isLoading ? <Spinner /> : ''}
                     <Modal.Header>
                         Add a Channel
-                </Modal.Header>
+                    </Modal.Header>
                     <Modal.Content>
-                        <Form>
+                        <Form onSubmit={this.onSubmit}>
                             <Form.Field>
                                 <Input
                                     fluid
+                                    className={classnames("", {
+                                        "error": isInValid
+                                    })}
                                     label="Name of Channel"
                                     name="channelName"
                                     onChange={this.onChange}
@@ -56,6 +111,9 @@ class Channels extends Component {
                             <Form.Field>
                                 <Input
                                     fluid
+                                    className={classnames("", {
+                                        "error": isInValid
+                                    })}
                                     label="About the Channel"
                                     name="channelDetails"
                                     onChange={this.onChange}
@@ -66,18 +124,26 @@ class Channels extends Component {
                     </Modal.Content>
 
                     <Modal.Actions>
-                        <Button color="green" inverted>
+                        <Button color="green" inverted onClick={this.onSubmit}>
                             <Icon name="checkmark" /> Add
-                </Button>
+                        </Button>
                         <Button color="red" inverted onClick={this.handleCloseModal}>
                             <Icon name="remove" /> Cancel
-                </Button>
+                        </Button>
                     </Modal.Actions>
-
+                    {this.isShownErrorMessage()}
                 </Modal>
             </React.Fragment>
         );
     }
 }
 
-export default Channels;
+Channels.propTypes = {
+    user: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+    user: state.users.user
+});
+
+export default connect(mapStateToProps, null)(Channels);
