020dabbfd9b384100c9e3b101a99ee9446d202f8
joker7t <batoan131196@gmail.com>
2020-03-07 10:58:36 +0700

    add picked color to firebase

--- src/components/color-panel/ColorPanel.js
+++ src/components/color-panel/ColorPanel.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
-import { Sidebar, Menu, Divider, Button, Modal, Icon, Label } from "semantic-ui-react";
+import { Sidebar, Menu, Divider, Button, Modal, Icon, Label, Segment } from "semantic-ui-react";
 import { SliderPicker } from "react-color";
+import firebase from "../../firebase";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 class ColorPanel extends Component {
     constructor() {
         super();
 
         this.state = {
-            modal: false
+            modal: false,
+            primary: '',
+            secondary: '',
+            usersRef: firebase.database().ref("users")
         }
     }
+
+    handleChangePrimary = (color) => this.setState({ primary: color.hex });
+
+    handleChangeSecondary = (color) => this.setState({ secondary: color.hex });
 
     openModal = () => this.setState({ modal: true });
 
     closeModal = () => this.setState({ modal: false });
 
+    handleSaveColors = () => {
+        const { primary, secondary } = this.state;
+        if (primary && secondary) {
+            this.saveColors(primary, secondary);
+        }
+    }
+
+    saveColors = (primary, secondary) => {
+        const { uid } = this.props.user;
+        this.state.usersRef
+            .child(`${uid}/colors`)
+            .push()
+            .update({
+                primary, secondary
+            })
+            .then(() => {
+                console.log("Color added");
+                this.closeModal();
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+    }
+
     render() {
-        const { modal } = this.state;
+        const { modal, primary, secondary } = this.state;
 
         return (
             <Sidebar
@@ -34,13 +69,18 @@ class ColorPanel extends Component {
                 <Modal basic open={modal} onClose={this.closeModal}>
                     <Modal.Header>Choose App Colors</Modal.Header>
                     <Modal.Content>
-                        <Label content="Primary Color" />
-                        <SliderPicker />
-                        <Label content="Secondary Color" />
-                        <SliderPicker />
+                        <Segment inverted>
+                            <Label content="Primary Color" />
+                            <SliderPicker color={primary} onChange={this.handleChangePrimary} />
+                        </Segment>
+
+                        <Segment inverted>
+                            <Label content="Secondary Color" />
+                            <SliderPicker color={secondary} onChange={this.handleChangeSecondary} />
+                        </Segment>
                     </Modal.Content>
                     <Modal.Actions>
-                        <Button color="green" inverted>
+                        <Button color="green" inverted onClick={this.handleSaveColors}>
                             <Icon name="checkmark" />Save Color
                         </Button>
 
@@ -54,4 +94,12 @@ class ColorPanel extends Component {
     }
 }
 
-export default ColorPanel;
+ColorPanel.propTypes = {
+    user: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    user: state.user.user,
+})
+
+export default connect(mapStateToProps, null)(ColorPanel);
